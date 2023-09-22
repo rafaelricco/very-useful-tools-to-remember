c19f4445a862fb3307d3205b00edd619a4483066
Douglas Lins <douglas_lins11@hotmail.com>
2023-09-22 16:27:35 -0300

    initial project structure

---
+++ src/main/kotlin/com/geotelles/veryusefultoolstoremember/VeryUsefulToolsToRememberApplication.kt
@@ -0,0 +1,11 @@
+package com.geotelles.veryusefultoolstoremember
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeryUsefulToolsToRememberApplication
+
+fun main(args: Array<String>) {
+	runApplication<VeryUsefulToolsToRememberApplication>(*args)
+}
