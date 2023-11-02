e02205dad6a9a14b69668a83ae89406b0a6329f0
Rafael Ricco <rafaelricco@gmail.com>
2023-11-02 14:26:51 -0300

    adding database support

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -3,9 +3,9 @@ CREATE SCHEMA IF NOT EXISTS very_useful_tools;
 create table IF NOT EXISTS very_useful_tools.very_useful_tools
 (
     id serial not null,
-    title varchar not null,
-    link varchar not null,
-    description varchar not null,
-    tags varchar not null
-    constraint id_pkey primary_key(id)
+    title text not null,
+    link text not null,
+    description text not null,
+    tags text not null,
+    constraint id_pkey primary key(id)
 )
